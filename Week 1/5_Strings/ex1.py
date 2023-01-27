8ff565bc0c237d703b93e9b1015aff0880518d6a
Ramatullaev <ramatullaev19@gmail.com>
2023-01-27 13:35:39 +0600

    Kit-kat

---
+++ Week 1/5_Strings/ex1.py
@@ -0,0 +1,16 @@
+a = "Hello"
+print(a)
+print()
+a = '''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
+print(a)
+
+#Strings are Arrays
+a = "Hello, World!"
+print(a[1])
+print()
+
+for x in "banana":
+  print(x)
