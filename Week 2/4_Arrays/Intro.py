388c0db8ff1482c9c13055aa577ab634ff47fecf
Ramatullaev <ramatullaev19@gmail.com>
2023-01-30 23:47:09 +0600

    Ocean

---
+++ Week 2/4_Arrays/Intro.py
@@ -0,0 +1,25 @@
+# Arrays
+# Arrays are used to store multiple values in one single variable:
+
+cars = ["Ford", "Volvo", "BMW"]
+
+
+# Get the value of the first array item:
+x = cars[0]
+
+# Modify the value of the first array item:
+cars[0] = "Toyota"
+
+
+'''Use the len() method to return the length of an 
+array (the number of elements in an array).'''
+x = len(cars)
+
+# Note: The length of an array is always one more than the highest array index.
+
+
+
+
+
+
+
---
+++ Week 2/4_Arrays/Looping arrays.py
@@ -0,0 +1,20 @@
+# Looping Array Elements
+'''You can use the for in loop to loop through all the elements of an array.'''
+
+# Print each item in the cars array:
+for x in cars:
+  print(x)
+
+
+# You can use the append() method to add an element to an array.
+'''Add one more element to the cars array:'''
+cars.append("Honda")
+
+# You can use the pop() method to remove an element from the array.
+# Delete the second element of the cars array:
+cars.pop(1)
+
+
+# You can also use the remove() method to remove an element from the array.
+'''Delete the element that has the value "Volvo":'''
+cars.remove("Volvo")
