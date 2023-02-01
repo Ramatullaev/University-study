39a45e24cca9dc1ff4013275694a7277283eb7b8
Ramatullaev <ramatullaev19@gmail.com>
2023-02-01 14:19:07 +0600

    casio

--- Week 2/1_Lists/Exercises.py/4.py
+++ Week 2/1_Lists/Exercises.py/4.py
@@ -1,1 +1,3 @@
 # Use the insert method to add "lemon" as the second item in the fruits list.
+fruits = ["apple", "banana", "cherry"]
+fruits.insert(1, "lemon")
--- Week 2/5_Tuple/Intro.py
+++ Week 2/5_Tuple/Intro.py
@@ -2,6 +2,7 @@
 
 ''''Tuple is one of 4 built-in data types in Python used to store collections of data, 
 the other 3 are List, Set, and Dictionary, all with different qualities and usage.'''
+
 thistuple = ("apple", "banana", "cherry")
 print(thistuple)
 
