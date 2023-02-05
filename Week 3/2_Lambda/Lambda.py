ffed623c0b0a29c2f1c9495b205206ee5d0108f8
Ramatullaev <ramatullaev19@gmail.com>
2023-02-05 19:00:34 +0600

    Dream

--- Week 3/2_Lambda/Lambda.py
+++ Week 3/2_Lambda/Lambda.py
@@ -1,16 +1,41 @@
-# Lambda
+ # Why Use Lambda Functions?
 
-'''A lambda function is a small anonymous function.
+'''The power of lambda is better shown when you use them as 
+an anonymous function inside another function.
 
-A lambda function can take any number of arguments, but can only have one expression.'''
+Say you have a function definition that takes one argument, and 
+that argument will be multiplied with an unknown number:'''
+def myfunc(n):
+    return lambda a : a * n
 
-# Syntax
-# lambda arguments : expression
-# The expression is executed and the result is returned:
+# Use that function definition to make a function that always 
+# doubles the number you send in:
+def myfunc(n):
+  return lambda a : a * n
 
-'Add 10 to argument a, and return the result:'
-x = lambda a : a + 10
-print(x(5))    # will be: 15
+mydoubler = myfunc(2)
 
+print(mydoubler(11))
+
+'''Or, use the same function definition to make a function 
+that always triples the number you send in:'''
+def myfunc(n):
+  return lambda a : a * n
+
+mytripler = myfunc(3)
+
+print(mytripler(11))
+
+# Or, use the same function definition to make both functions, in the same program:
+def myfunc(n):
+  return lambda a : a * n
+
+mydoubler = myfunc(2)
+mytripler = myfunc(3)
+
+print(mydoubler(11)) 
+print(mytripler(11))
+
+# Use lambda functions when an anonymous function is required for a short period of time.
 
 
