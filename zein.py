e6339749c5cc537494e30f8b067d35dd987f321c
Ramatullaev <ramatullaev19@gmail.com>
2023-05-12 12:20:42 +0600

    change

--- zein.py
+++ zein.py
@@ -1,21 +1,21 @@
-class MyNumbers:
-def __iter__(self):
-    self.a = 1
-return self
-(self):
-def
-next
-X
-=
-self.a
-self.a
-+= 1
-return X
-(self):
-myclass
-= MyNumbers ( )
-miter = iter(myclass)
-next (myiter)
-print(next (myiter))
-print(next (myiter))
-print (next (myiter))
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Constants
+gamma = 1.4  # Specific heat ratio for an ideal diatomic gas
+R = 8.314   # Gas constant (J/(mol*K))
+n = 1       # Number of moles of the gas
+
+# Temperature range
+T = np.linspace(100, 1000, 100)  # Range of temperature values in Kelvin
+
+# Calculate pressure values for the adiabatic curve
+P = (n * R * T) ** (1 / gamma)
+
+# Plotting
+plt.plot(T, P)
+plt.xlabel('Temperature (K)')
+plt.ylabel('Pressure (Pa)')
+plt.title('Adiabatic Curve on PT Graph')
+plt.grid(True)
+plt.show()
